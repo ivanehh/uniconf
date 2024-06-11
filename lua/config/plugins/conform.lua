ef9976866010fbeb4610c6d41793954d9f4478d5
Ivan Terziev <ivan.terziev@rdm.com>
2024-06-11 08:14:46 +0300

    added yamlsync

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt' },
+      yaml = { 'yamlfix' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
